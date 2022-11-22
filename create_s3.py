f3749577380a919f2542d5c697b7f0421f96b926
RomuloPaiva <RomuloP@starsgroup.com>
2022-11-22 18:59:23 +0000

    dfd
    df

---
+++ create_s3.py
@@ -0,0 +1,12 @@
+import boto3
+import os
+
+
+# create s3 bucket
+s3_client = boto3.client('s3',
+                            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+                            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+                            region_name="eu-region-01")
+
+_ = s3_client.create_bucket(Bucket="romulo-amaral-bucket-codepipeline",
+                            CreateBucketConfiguration={'LocationConstraint': "eu-region-01"})
